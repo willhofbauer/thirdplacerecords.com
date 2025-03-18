a62897cb48a9ee849f2a2d96e3aee90b9169c7d8
willhofbauer <willhofbauer@gmail.com>
2025-03-18 21:37:12 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,8 +42,10 @@ export default function RootLayout({
           <Link href="/" className="block mx-auto w-fit hover:animate-wobble">
             <Image
               src="/website-logo.png"
-              {/* src="https://qiumoxf8shroy7uq.public.blob.vercel-storage.com/website-logo-k7yjoNvspPHaHzio482xFfpmrgu3Jv.png"*/}
-              {/*src="https://raw.githubusercontent.com/willhofbauer/thirdplacerecords.com/blob/main/public/website-logo.png"*/}
+              {/* Old URLs commented out:
+              src="https://qiumoxf8shroy7uq.public.blob.vercel-storage.com/website-logo-k7yjoNvspPHaHzio482xFfpmrgu3Jv.png"
+              src="https://raw.githubusercontent.com/willhofbauer/thirdplacerecords.com/blob/main/public/website-logo.png"
+              */}
               alt="Third Place Records Logo"
               width={260}
               height={130}
